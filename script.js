f193ac382dd1bc11f03ab80aacac265c43779d09
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2023-11-01 01:40:09 +0530

    refactoring

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let SendHandler = () => {
-    let name=document.getElementById("name");
-    let email=document.getElementById("email");
-    let message=document.getElementById("message");
+let sendHandler = () => {
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let message = document.getElementById("message");
 
     
 
-    name.value="";
-    email.value="";
-    message.value="";
+    name.value = "";
+    email.value = "";
+    message.value = "";
 }
